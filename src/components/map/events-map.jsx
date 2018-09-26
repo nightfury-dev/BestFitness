74907accea37294088c7dfd95af774438e0bf6c0
NightFury <82142029+nightfury-dev@users.noreply.github.com>
2018-09-26 00:57:19 +0000

    merging

--- src/components/map/events-map.jsx
+++ src/components/map/events-map.jsx
@@ -11,7 +11,13 @@ export class MapContainer extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      mapData: []
+      mapData: [],
+      showingInfoWindow: false,
+      selectedPlace: {},
+      name: '',
+      description: '',
+      datetime: '',
+      loading: true
     }
   }
 
@@ -21,17 +27,32 @@ export class MapContainer extends Component {
         const data = response.data;
         console.log("data is", data)
         const oldMapData = this.state.mapData
+
+
         const newMapData = []
+
         data.forEach((location) => {
           newMapData.push(coordinates(location))
         })
-        this.setState( {mapData: [...newMapData, ...oldMapData]} )
+
+        this.setState( {mapData: [...newMapData, ...oldMapData], loading: false} )
+
       })
+  }
+
+  onMarkerClick = (props, marker, event) => {
+    this.setState({
+      selectedPlace: props,
+      activeMarker: marker,
+      showingInfoWindow: true
+    });
   }
 
 
 
+
   render() {
+    console.log("this.state.selectedPlace",this.state.selectedPlace);
     const generateMapMarkers = this.state.mapData.map ((marker, index) => {
       return <Marker position={marker} key={index} onClick = {generateInfoWindow}/>
     })
@@ -41,9 +62,21 @@ export class MapContainer extends Component {
       return <InfoWindow visible={true} />
     })
 
+  console.log("this.state.mapData[0]", this.state.mapData[0]);
 
+  const myPlacesArray = this.state.mapData;
+  console.log("myPlacesArray",myPlacesArray);
 
-    console.log(generateMapMarkers);
+  const mapInfos = !myPlacesArray ? null : myPlacesArray.filter(event => event.name === this.state.selectedPlace.name).map((mapInfo) => {
+    return (
+      <div>
+
+        <h3>{ mapInfo.name }</h3>
+        <p>{mapInfo.description }</p>
+        <p>{mapInfo.datetime}</p>
+
+      </div>)
+  });
 
     return (
       <main>
@@ -54,15 +87,31 @@ export class MapContainer extends Component {
           initialCenter={{lat: 43.6446002, lng: -79.3951586}}
         >
           {generateMapMarkers}
+          <InfoWindow onClose={this.onInfoWindowClose}
+                      marker={this.state.activeMarker}
+                      visible={this.state.showingInfoWindow}
+          >
+          <div>
+            {mapInfos}
+          </div>
+
+          </InfoWindow>
+
         </Map>
       </main>
     );
   }
+
 }
 
 export default GoogleApiWrapper({
   apiKey: (process.env.REACT_APP_GOOGLE_API_KEY)
 })(MapContainer)
+
+/*
+const myPlaces = this.state.mapData
+const toRender = myPlaces.map()
+*/
 
 
 
